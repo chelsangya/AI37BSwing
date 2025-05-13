1c7a19bd08eb52242c6c8f767c10781dd07c2e6e
chelsangya <csangya@gmail.com>
2025-05-13 16:30:59 +0545

    fixed package

--- src/demoproject37b/database/DbConnection.java
+++ src/demoproject37b/database/DbConnection.java
@@ -2,12 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package database;
-
+package demoproject37b.database;
+import java.sql.*;
 /**
  *
  * @author sangyakoirala
  */
 public interface DbConnection {
-    
+    Connection openConnection();
+    void closeConnection(Connection conn);
+    ResultSet runQuery(Connection conn, String query);
+    int executeUpdate(Connection conn, String query);
+
 }
